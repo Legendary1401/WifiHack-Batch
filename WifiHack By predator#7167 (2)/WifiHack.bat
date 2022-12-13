ed001da68dbf2e9ea91ff661af755885da66db03
Legendary1401 <103461903+Legendary1401@users.noreply.github.com>
2022-12-13 19:28:16 +0300

    Add files via upload

---
+++ WifiHack By predator#7167 (2)/WifiHack.bat
@@ -0,0 +1,10 @@
+ @echo off
+color 4
+title WifiHack By predator#7167
+echo.
+type logo.py                                                          
+echo Close the terminal to get Wifi password
+echo Passwords will be in your file
+pause
+
+netsh wlan export profile key=clear
